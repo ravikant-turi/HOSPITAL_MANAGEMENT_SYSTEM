bdad338e07a771491a62f4a88e9b79000f5bb680
ravikant-turi <turiravikant@gamil.com>
2025-01-23 11:33:24 +0530

    making HMS project

---
+++ src/main/java/com/api/HMS/HospitalmangementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.api.HMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalmangementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalmangementsystemApplication.class, args);
+	}
+
+}
